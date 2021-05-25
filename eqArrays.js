e74374e38b20227d69aa9ecba7a2a47cbeca16cb
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-25 22:51:30 +0000

    Implemented eqArrays.js for comparing arrays

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+/* 
+Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+*/
+
+//require nod-emoji package for emojis
+const emoji = require('node-emoji');
+
+const assertEqual = function(actual, expected) {
+  //get check mark emoji
+  const emojiSuccess = emoji.get('white_check_mark');
+  //get stop emoji
+  const emojiFail = emoji.get('octagonal_sign');
+  if (actual === expected) {
+    console.log(`${emojiSuccess + emojiSuccess + emojiSuccess}Assertion Passed: [${actual}] === [${expected }]`);
+  } else {
+    console.log(`${emojiFail + emojiFail + emojiFail}Assertion Failed: [${actual}] !== [${expected }]`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+//TEST CASES:
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 3]), false); 
+assertEqual(eqArrays([1, 2, 3], [3, 2]), false);
+assertEqual(eqArrays([1, 2], [3, 2, 3]), false);
+assertEqual(eqArrays([], [3, 2, 3]), false);
+assertEqual(eqArrays([], []), true);
