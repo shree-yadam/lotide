ac7bda10ac008008680ec41f1b34f1c118f4595b
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-25 23:00:06 +0000

    Implemented assertArrayEqual.js to test equality of two arrays

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,6 +2,9 @@
 Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 */
 
+//require nod-emoji package for emojis
+const emoji = require('node-emoji');
+
 const eqArrays = function(arr1, arr2) {
   if (arr1.length === arr2.length) {
     for (let i = 0; i < arr1.length; i++) {
@@ -13,3 +16,17 @@ const eqArrays = function(arr1, arr2) {
   }
   return false;
 };
+
+const assertArrayEqual = function(actual, expected) {
+  //get check mark emoji
+  const emojiSuccess = emoji.get('white_check_mark');
+  //get stop emoji
+  const emojiFail = emoji.get('octagonal_sign');
+  if (eqArrays(actual, expected)) {
+    console.log(`${emojiSuccess + emojiSuccess + emojiSuccess}Assertion Passed: [${actual}] === [${expected }]`);
+  } else {
+    console.log(`${emojiFail + emojiFail + emojiFail}Assertion Failed: [${actual}] !== [${expected }]`);
+  }
+};
+
+assertArrayEqual([1,2, 3], [1,2,3]);
