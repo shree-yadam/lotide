8881beb9b1923e1fe4570bb6dce0f1a7d0effc0f
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-26 19:31:36 +0000

    Implemented count letters to find the letter count of all letters in a string

---
+++ countLetters.js
@@ -0,0 +1,41 @@
+//require nod-emoji package for emojis
+const emoji = require('node-emoji');
+
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  //get check mark emoji
+  const emojiSuccess = emoji.get('white_check_mark');
+  //get stop emoji
+  const emojiFail = emoji.get('octagonal_sign');
+  if (actual === expected) {
+    console.log(`${emojiSuccess + emojiSuccess + emojiSuccess}Assertion Passed: [${actual}] === [${expected }]`);
+  } else {
+    console.log(`${emojiFail + emojiFail + emojiFail}Assertion Failed: [${actual}] !== [${expected }]`);
+  }
+};
+
+const countLetters = function(str) {
+  const result = {};
+  for (let elem of str) {
+    if (elem !== " ") {
+      if(result[elem.toLowerCase()]) {
+        result[elem.toLowerCase()] += 1;
+      } else {
+        result[elem.toLowerCase()] = 1;
+      }
+    }
+  }
+  return result;
+};
+
+const result = countLetters("lighthouse in the house");
+assertEqual(result['l'], 1);
+assertEqual(result['i'], 2);
+assertEqual(result['g'], 1);
+assertEqual(result['h'], 4);
+assertEqual(result['t'], 2);
+assertEqual(result['o'], 2);
+assertEqual(result['u'], 2);
+assertEqual(result['s'], 2);
+assertEqual(result['e'], 3);
+assertEqual(result['n'], 1);
