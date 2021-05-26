e7b28b8c30c1ff4483aa935f053eebb61379800f
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-26 17:15:29 +0000

    Implemented middle for finding middle elements of array

---
+++ middle.js
@@ -0,0 +1,51 @@
+/* Implement middle which will take in an array and return the middle-most element(s) of the given array. */
+
+//require nod-emoji package for emojis
+const emoji = require('node-emoji');
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length === arr2.length) {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  //get check mark emoji
+  const emojiSuccess = emoji.get('white_check_mark');
+  //get stop emoji
+  const emojiFail = emoji.get('octagonal_sign');
+  if (eqArrays(actual, expected)) {
+    console.log(`${emojiSuccess + emojiSuccess + emojiSuccess}Assertion Passed: [${actual}] === [${expected }]`);
+  } else {
+    console.log(`${emojiFail + emojiFail + emojiFail}Assertion Failed: [${actual}] !== [${expected }]`);
+  }
+};
+
+const middle = function(arr) {
+  if (arr.length <= 2 || arr === undefined) {
+    return [];
+  }
+  const result = [];
+  if(arr.length% 2 !== 0) {
+    result.push(arr[(arr.length-1)/2]);
+  } else {
+    result.push(arr[arr.length/2 - 1]);
+    result.push(arr[arr.length/2]);
+  }
+  return result;
+};
+
+//TEST CASES
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
