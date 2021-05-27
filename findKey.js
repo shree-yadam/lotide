783ff036dff8131fa4a7709e8d223ab14bce878e
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-27 21:55:14 +0000

    Implemented findKey function with callback

---
+++ findKey.js
@@ -0,0 +1,61 @@
+/*
+Implement the function findKey which takes in an object and a callback. It should scan the object and return the first key for which the callback returns a truthy value. If no key is found, then it should return undefined.
+*/
+
+//require nod-emoji package for emojis
+const emoji = require('node-emoji');
+
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  //get check mark emoji
+  const emojiSuccess = emoji.get('white_check_mark');
+  //get stop emoji
+  const emojiFail = emoji.get('octagonal_sign');
+  if (actual === expected) {
+    console.log(`${emojiSuccess + emojiSuccess + emojiSuccess}Assertion Passed: [${actual}] === [${expected }]`);
+  } else {
+    console.log(`${emojiFail + emojiFail + emojiFail}Assertion Failed: [${actual}] !== [${expected }]`);
+  }
+};
+
+function findKey(obj, callback) {
+  for (let elem in obj) {
+    if (callback(obj[elem])) {
+      return elem;
+    }
+  }
+  return undefined;
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));  // => "noma"
+
+//TEST CASES with assertEqual
+//General
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x  => x.stars > 2), "Akaleri");
+
+//Empty object
+assertEqual(findKey({}, x => x.name === "rain"), undefined);
+
+//Truthy value
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, ()  => 3), "Blue Hill");
