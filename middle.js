d8e0f1a5fa04c504049d255ee504551b37fb0486
Shree Devi Yadam <shree.yadam@gmail.com>
2021-05-31 17:35:28 +0000

    refactored code for middle.js and separated test cases into test folder

--- middle.js
+++ middle.js
@@ -1,27 +1,5 @@
 /* Implement middle which will take in an array and return the middle-most element(s) of the given array. */
 
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length === arr2.length) {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-  return false;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  const emojiSuccess = "✅✅✅";
-  const emojiFail = "🛑🛑🛑";
-  if (eqArrays(actual, expected)) {
-    console.log(`${emojiSuccess}Assertion Passed: [${actual}] === [${expected }]`);
-  } else {
-    console.log(`${emojiFail}Assertion Failed: [${actual}] !== [${expected }]`);
-  }
-};
-
 const middle = function(arr) {
   if (arr.length <= 2 || arr === undefined) {
     return [];
@@ -36,11 +14,4 @@ const middle = function(arr) {
   return result;
 };
 
-//TEST CASES
-assertArraysEqual(middle([]), []);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
